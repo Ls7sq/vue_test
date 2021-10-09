857c7bdb41f3f25bed478fa9b00de2bc42fef557
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-09 12:54:10 +0800

    引入完整版的vue

--- src/main.js
+++ src/main.js
@@ -2,15 +2,16 @@
   该文件是整个项目的入口文件
 */
 //引入Vue
-import Vue from 'vue'
+import Vue from 'vue/dist/vue'
 //引入App组件，它是所有组件的父组件
-import App from './App.vue'
+//import App from './App.vue'
 //关闭vue生产提示
 Vue.config.productionTip = false
 
 //创建Vue实例对象----vm
 new Vue({
   el:"#app",
+  template:`<h1>Test</h1>`
   //完成将App放入容器中的功能
-  render: h => h(App),
+  //render: h => h(App),
 })
