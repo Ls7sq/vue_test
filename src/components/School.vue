893af32bcdb977b8c185433f5f750a00d1be523f
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-09 12:43:58 +0800

    initialize  CLI

---
+++ src/components/School.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- 组件的结构 -->
+  <div class="demo">
+    <h2>学校名称：{{name}}</h2>	
+    <h2>学校地址：{{address}}</h2>
+    <button @click="showName">Click me</button>
+  </div>
+</template>
+
+<script>
+    const school = {
+        name:'School',
+        data() {
+            return {
+                name:"school",
+                address:"adress"
+            }
+        },
+        methods: {
+            showName(){
+                alert(this.name)
+                console.info(this)
+            }
+        },
+    }
+    export default school
+//组件交互相关的代码(数据，方法)
+</script>
+
+<style>
+/* 组件的样式 */
+    .demo{
+        background-color: aliceblue;
+    }
+</style>
---
+++ src/components/Student.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- 组件的结构 -->
+  <div>
+    <h2>学生名称：{{name}}</h2>	
+    <h2>学生地址：{{address}}</h2>
+  </div>
+</template>
+
+<script>
+    const student = {
+        name:'Student',
+        data() {
+            return {
+                name:"student",
+                address:"adress"
+            }
+        }
+    }
+    export default student
+//组件交互相关的代码(数据，方法)
+</script>
