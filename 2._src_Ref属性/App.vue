ebc49a33665931384aa51a9b7b7385c3e9482842
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-09 15:19:42 +0800

    ref属性

---
+++ 2._src_Ref属性/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1 v-text="msg" ref="title"></h1>
+    <button ref="btn" @click="showDOM">Please click me</button>
+    <School ref="sch" />
+
+  </div>
+</template>
+
+<script>
+//引入School组件
+import School from './components/School.vue'
+
+export default {
+    name:'App',
+    components:{School},
+    data() {
+      return {
+        msg:"Welcome"
+      }
+    },
+    methods:{
+      showDOM(){
+
+        console.log(this.$refs)
+        console.log("Current this")
+        console.log(this)
+
+        console.log("DOM h1: ")
+        console.log(this.$refs.title)//真实的DOM元素
+
+        console.log("DOM button: ")
+        console.log(this.$refs.btn)//真实的DOM元素
+
+        console.log("VueComponent: ")
+        console.log(this.$refs.sch)//School组件的实例对象(vc)
+      }
+    }
+}
+</script>
---
+++ 2._src_Ref属性/components/School.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="school">
+      <h2>School Name - {{name}}</h2>
+      <h2>School Address - {{address}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'School',
+    data() {
+        return {
+            name:"shiqi",
+            address:"chengdu"
+        }
+    },
+
+}
+</script>
+
+<style>
+    .school{
+        background-color: beige;
+    }
+</style>
