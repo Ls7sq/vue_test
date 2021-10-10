4e7730d1671bf9d642304ae0c2281b286c88bb94
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-10 21:23:33 +0800

    globalEventBus

---
+++ src/components/School.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="school">
+      <h2>School Name - {{name}}</h2>
+      <h2>School Address - {{address}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'School',
+    props:['getSchoolName'],
+    data() {
+        return {
+            name:"NUS12345",
+            address:"Singapore"
+        }
+    },
+    mounted() {
+        //console.log("school",this.x)
+        
+        //School给x绑定了一个事件叫做hello,School自身有个函数作为该hello事件如果被触发时，调用在School里的函数
+        //该函数还接受一个参数data
+        this.$bus.$on('hello',(data)=>{
+            console.log("school component, receive data", data)
+        })
+    },
+    beforeDestroy(){
+        this.$bus.$off('hello')
+    }
+}
+</script>
+
+<style scoped>
+    .school{
+        background-color: orange;
+        padding: 5px;
+    }
+</style>
---
+++ src/components/Student.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="student">
+      <h2>Student Name - {{name}}</h2>
+      <h2>Student Sex - {{sex}}</h2>
+      <button @click="sendStudentName">Send Student name to School component</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Student',
+    data() {
+        return {
+            name:"zhangsan",
+            sex:"M",
+        }
+    },
+    methods: {
+        sendStudentName(){
+            //Student触发x身上的hello事件，接下来Student传递一个参数666去回调在School里的函数
+            this.$bus.$emit('hello',this.name)
+        }
+    },
+    mounted() {
+        //console.log("student",this.x)
+    },
+}
+</script>
+
+<style lang="less">
+    .student{
+        background-color:pink;
+        padding: 5px;
+        margin-top: 30px;
+    }
+</style>
