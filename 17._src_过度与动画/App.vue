e5f503d2acf684815fc66c7ff777564a94589dde
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-11 20:49:38 +0800

    TodoList animation

---
+++ 17._src_过度与动画/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Test/>
+    <Test2/>
+    <Test3/>
+  </div>
+</template>
+
+<script>
+import Test from './components/test.vue'
+import Test2 from './components/test2.vue'
+import Test3 from './components/test3.vue'
+
+
+export default {
+    name:'App',
+    components:{Test,Test2,Test3},
+
+}
+</script>
