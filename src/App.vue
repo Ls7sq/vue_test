74824bf8f0ed3ddeb2fb6e952512d8f7a5e53e45
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-12 00:27:21 +0800

    github 静态组件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>
-  </div>
+    <div class="container">
+      <Search/>
+      <List/>
+    </div>
 </template>
 
 <script>
+import Search from './components/Search.vue'
+import List from './components/List.vue'
 export default {
     name:'App',
+    components:{Search,List},
     methods: {
     },
 }
