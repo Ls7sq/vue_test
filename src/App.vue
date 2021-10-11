bf1ad7ec7d18ae2462c2971ea8ccb85efb71f99c
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-11 20:21:45 +0800

    animate.css

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,19 @@
   <div>
     <Test/>
     <Test2/>
+    <Test3/>
   </div>
 </template>
 
 <script>
 import Test from './components/test.vue'
 import Test2 from './components/test2.vue'
+import Test3 from './components/test3.vue'
 
 
 export default {
     name:'App',
-    components:{Test,Test2},
+    components:{Test,Test2,Test3},
 
 }
 </script>
---
+++ src/components/test3.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <button @click="isShow = !isShow">显示/隐藏</button>
+        <transition-group 
+            name="animate__animated animate__bonunce" 
+            enter-active-class="animate__swing"
+            leave-active-class="animate__backOutUp"
+            appear>
+            <h1 v-show="!isShow" key="1">hello</h1>
+            <h1 v-show="isShow" key="2">hello</h1>
+        </transition-group>
+    </div>
+</template>
+
+<script>
+import 'animate.css'
+export default {
+    name:'Test',
+    data() {
+        return {
+            isShow:true
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+    h1{
+        background-color: orange;
+    }
+
+
+</style>
