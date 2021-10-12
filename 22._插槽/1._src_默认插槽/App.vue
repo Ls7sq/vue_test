7e7513791451c5f9980159aad5cbcd484c69673b
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-12 22:43:00 +0800

    ignore

---
+++ 22._插槽/1._src_默认插槽/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+      <Category title="Food">
+        <img src="https://s3.ax1x.com/2021/01/16/srJlq0.jpg" alt="">
+      </Category>
+      <Category title="Game">
+        <ul>
+            <li v-for="(game, index) in games" :key="index">{{game}}</li>
+        </ul>
+      </Category>
+
+      <Category title="Films">
+        <video controls src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"></video>
+      </Category>
+    </div>
+</template>
+
+<script>
+import Category from './components/Category.vue'
+
+export default {
+    name:'App',
+    components:{Category},
+    data() {
+      return {
+        foods:['火锅','烧烤','小龙虾','牛排'],
+				games:['红色警戒','穿越火线','劲舞团','超级玛丽'],
+				films:['《教父》','《拆弹专家》','《你好，李焕英》','《尚硅谷》']
+      }
+    },
+    methods: {
+    },
+}
+</script>
+
+<style lang="css">
+  .container{
+    display: flex;
+    justify-content: space-around;
+  }
+</style>
