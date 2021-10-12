15947f58b539062c1324a14c83bdd5efc74253ad
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-12 08:21:37 +0800

    github api

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -2,14 +2,32 @@
     <section class="jumbotron">
         <h3 class="jumbotron-heading">Search Github Users</h3>
         <div>
-            <input type="text" placeholder="enter the name you search"/>&nbsp;<button>Search</button>
+            <input type="text" placeholder="enter the name you search" v-model="keyWord"/>&nbsp;
+            <button @click="searchUsers">Search</button>
         </div>
     </section>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    name:'Search'
+    name:'Search',
+    data() {
+        return {
+            keyWord:''
+        }
+    },
+    methods:{
+        searchUsers(){
+            axios.get(`https://api.github.com/search/users?q=${this.keyWord}`)
+                 .then(response=>{
+                     console.log('Successful')
+                     this.$bus.$emit('getUsers', response.data.items)
+                 },error=>{
+                     console.log('Failed', error.message)
+                 })
+        }
+    }
 
 }
 </script>
