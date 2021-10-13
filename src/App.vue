9a21b82be675a1936ef2d8b849aa3d055e2d4be2
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-13 16:20:01 +0800

    ignore

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
     <div class="container">
-      <Category title="Food">
-        <img slot="center" src="https://s3.ax1x.com/2021/01/16/srJlq0.jpg" alt="">
-        <a slot="footer" href="http://youtube.com">Click me to link</a>
-      </Category>
+
       <Category title="Game">
         <ul slot="center">
             <li v-for="(game, index) in games" :key="index">{{game}}</li>
@@ -14,18 +11,6 @@
         </div>
       </Category>
 
-      <Category title="Films">
-        <video slot="center" controls src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"></video>
-        <template v-slot:footer>
-         <div class="foot">
-            <a href="">1</a>
-            <a href="">2</a>
-            <a href="">3</a>
-          </div>
-          <h4>welcome</h4>
-        </template>
-
-      </Category>
     </div>
 </template>
 
@@ -37,9 +22,7 @@ export default {
     components:{Category},
     data() {
       return {
-        foods:['火锅','烧烤','小龙虾','牛排'],
-				games:['红色警戒','穿越火线','劲舞团','超级玛丽'],
-				films:['《教父》','《拆弹专家》','《你好，李焕英》','《尚硅谷》']
+				games:['红色警戒','穿越火线','劲舞团','超级玛丽']
       }
     },
     methods: {
