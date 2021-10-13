3cb78aa66e369a7370dc6bbf1532d72740b138ec
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-13 20:46:54 +0800

    求和 Vuex版本

--- src/store/index.js
+++ src/store/index.js
@@ -7,13 +7,39 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 //准备actions-用于响应组件中的动作
-const actions = {}
+const actions = {
+    // jia:function(context,val){
+    //     context.commit('JIA', val)
+    // },
+    // jian(context, val){
+    //     context.commit('JIAN',val)
+    // },
+    jiaOdd(context, val){
+        if(context.state.sum % 2){
+            context.commit('JIA',val)
+        } 
+    },
+    jiaWait(context, val){
+        setTimeout(()=>{
+            context.commit('JIA',val)
+        },500)
+    }
+}
 
 //准备mutations-用于操作数据(state)
-const mutations = {}
+const mutations = {
+    JIA:function(state, val){
+        state.sum += val
+    },
+    JIAN(state, val){
+        state.sum -= val
+    }
+}
 
 //准备state-用于存储数据
-const state = {}
+const state = {
+    sum:0,//当前的数字
+}
 
 //创建store
 const store = new Vuex.Store({
