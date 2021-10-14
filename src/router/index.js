b08cf47b375b5fb99571ead036afd47b72837599
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-14 18:48:53 +0800

    路由的props配置

--- src/router/index.js
+++ src/router/index.js
@@ -30,8 +30,26 @@ const router = new VueRouter({
                     children:[
                         {
                             name:'xiangqing',
-                            path:'detail/:id/:title',
-                            component:Detail
+                            path:'detail',
+                            component:Detail,
+                            //props第一种写法：值为对象，该对象中的key-value都会以props的形式传给Detail组件
+                            //props:{}
+
+                            //props第二种写法：值为布尔值，若布尔值为真，就会把该路由组件收到的所有params参数，以props的形式传给Detail组件
+                            //props:true
+
+                            //props第三种写法：值为函数
+                            // props($route){
+                            //     return {id:$route.query.id, title:$route.query.title}
+                            // }
+
+                            //结构赋值
+                            // props({query:{id,title}}){
+                            //     return {id, title}
+                            // }
+                            props({query}){
+                                return {id: query.id, title: query.title}
+                            }
                         },
                         
                     ]
