fe117a2775c11b88423c146173cd6699fa0583cd
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-14 20:39:20 +0800

    生命周期 activated, deactivated

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -10,8 +10,11 @@
             <router-link class="list-group-item " active-class="active" to="/home/message">Message</router-link>
           </li>
         </ul>
-                      <!-- 组件名字 -->
-        <keep-alive include="News">
+        <!-- 缓存多个路由组件 -->
+        <!-- <keep-alive :include="['News','Message']"> -->
+          
+          <!-- 缓存一个 -->
+          <keep-alive include='News'>
           <router-view></router-view>
         </keep-alive>
       </div>
--- src/pages/News.vue
+++ src/pages/News.vue
@@ -1,6 +1,7 @@
 <template>
    <div>
         <ul>
+          <li :style="{opacity}">welcome vue</li>
           <li>news001 <input type="text"></li>
           <li>news002 <input type="text"></li>
           <li>news003 <input type="text"></li>
@@ -11,9 +12,34 @@
 <script>
 export default {
     name:'News',
-    beforeDestroy(){
-      console.log("news组件即将被销毁")
-    }
+    data() {
+      return {
+        opacity:1
+      }
+    },
+    // beforeDestroy(){
+    //   console.log("news组件即将被销毁")
+    //   clearInterval(this.timer)
+    // },
+    // mounted() {
+    //   setInterval(()=>{
+    //     this.timer = this.opacity -= 0.01
+    //     if (this.opacity <= 0) {
+    //       this.opacity = 1
+    //     }
+    //   },16)
+    // },
+    activated() {
+      setInterval(()=>{
+        this.timer = this.opacity -= 0.01
+        if (this.opacity <= 0) {
+          this.opacity = 1
+        }
+      },16)      
+    },
+    deactivated() {
+      clearInterval(this.timer)
+    },
 }
 </script>
 
