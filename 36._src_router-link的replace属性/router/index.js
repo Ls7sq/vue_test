7391b34753acc809a443f0585107a16c49f9a774
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-14 19:21:39 +0800

    ignore

---
+++ 36._src_router-link的replace属性/router/index.js
@@ -0,0 +1,62 @@
+//该文件专门用于创建整个应用的路由器
+import VueRouter from 'vue-router'
+
+//引入组件
+import About from '../pages/About'
+import Home from '../pages/Home'
+import News from '../pages/News'
+import Message from '../pages/Message'
+import Detail from '../pages/Detail'
+
+//创建一个路由器
+const router = new VueRouter({
+    routes: [
+        {
+            name:'关于',
+            path:'/about',
+            component:About
+        },
+        {
+            path:'/home',
+            component:Home,
+            children:[
+                {
+                    path:'news',
+                    component:News
+                },
+                {
+                    path:'message',
+                    component:Message,
+                    children:[
+                        {
+                            name:'xiangqing',
+                            path:'detail',
+                            component:Detail,
+                            //props第一种写法：值为对象，该对象中的key-value都会以props的形式传给Detail组件
+                            //props:{}
+
+                            //props第二种写法：值为布尔值，若布尔值为真，就会把该路由组件收到的所有params参数，以props的形式传给Detail组件
+                            //props:true
+
+                            //props第三种写法：值为函数
+                            // props($route){
+                            //     return {id:$route.query.id, title:$route.query.title}
+                            // }
+
+                            //结构赋值
+                            // props({query:{id,title}}){
+                            //     return {id, title}
+                            // }
+                            props({query}){
+                                return {id: query.id, title: query.title}
+                            }
+                        },
+                        
+                    ]
+                }
+            ]
+        },
+    ]
+})
+//暴露一个路由器
+export default router
