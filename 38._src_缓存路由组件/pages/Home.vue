11923c1067b83ac34b8e74c3c65a87b831cf8b0a
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-14 20:00:59 +0800

    ignore

---
+++ 38._src_缓存路由组件/pages/Home.vue
@@ -0,0 +1,35 @@
+<template>
+   <div>
+      <h2>Home组件内容</h2>
+      <div>
+        <ul class="nav nav-tabs">
+          <li>
+            <router-link class="list-group-item " active-class="active" to="/home/news">News</router-link>
+          </li>
+          <li>
+            <router-link class="list-group-item " active-class="active" to="/home/message">Message</router-link>
+          </li>
+        </ul>
+                      <!-- 组件名字 -->
+        <keep-alive include="News">
+          <router-view></router-view>
+        </keep-alive>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Home',
+        beforeDestroy(){
+          console.log('Home组件要被销毁了')
+        },
+        mounted() {
+          console.log('Home组件挂载完毕了')
+        },
+    }
+</script>
+
+<style>
+
+</style>
