241d2b66321016308fa4ce5ed6e79b55616b9997
Ls7sq <73803389+Ls7sq@users.noreply.github.com>
2021-10-15 10:58:29 +0800

    element ui

--- src/main.js
+++ src/main.js
@@ -2,14 +2,15 @@
 import Vue from 'vue'
 //引入App
 import App from './App.vue'
-//引入
-import VueRouter from 'vue-router'
 
-//引入路由器
-import router from './router/index'
+//引入ElementUI组件库
+import ElementUI from 'element-ui'
+//引入ElementUI全部样式
+import 'element-ui/lib/theme-chalk/index.css'
 
-//应用插件
-Vue.use(VueRouter)
+
+//应用ElementUI
+Vue.use(ElementUI)
 
 //关闭Vue的生产提示
 Vue.config.productionTip = false
@@ -18,8 +19,4 @@ Vue.config.productionTip = false
 new Vue({
     el:"#app",
     render:h=>h(App),
-    router:router,
-    beforeCreate() {
-        Vue.prototype.$bus = this
-    },
 })
